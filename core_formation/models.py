b6317dedc4ab40ae98549c8ec0a93cfb126553bb
Sanghyuk Moon <sanghyuk.moon@princeton.edu>
2025-04-18 08:53:07 -0400

    Add models

--- core_formation/models.py
+++ core_formation/models.py
@@ -1,5 +1,5 @@
 M5N512 = {f"M5J2P{iseed}N512": f"/scratch/gpfs/sm69/cores/hydro/M5.J2.P{iseed}.N512" for iseed in range(0, 40)}
-M5N1024 = {f"M5J2P{iseed}N1024": f"/scratch/gpfs/sm69/cores/hydro/M5.J2.P{iseed}.N1024" for iseed in range(0, 40)}
+M5N1024 = {f"M5J2P{iseed}N1024": f"/scratch/gpfs/sm69/cores/hydro/M5.J2.P{iseed}.N1024" for iseed in range(0, 20)}
 m2 = {f"M10J4P{iseed}N1024": f"/scratch/gpfs/sm69/cores/hydro/M10.J4.P{iseed}.N1024" for iseed in range(0, 2)}
 m3 = {f"M10J4P{iseed}N1024": f"/projects2/EOSTRIKE/sanghyuk/cores/M10.J4.P{iseed}.N1024" for iseed in range(2, 7)}
 M10N1024 = {**m2, **m3}
@@ -13,7 +13,7 @@ hydro_old = hydro.copy()
 hydro_old['M15J2P1N512'] = "/projects2/EOSTRIKE/sanghyuk/cores/M15.J2.P1.N512"
 hydro_old['M3J4P1N1024'] = "/tigress/sm69/cores/hydro/M3.J4.P1.N1024"
 
-mhd = {f"M10J4B4P{iseed}N1024": f"/scratch/gpfs/sm69/cores/mhd/M10.J4.B4.P{iseed}.N1024" for iseed in range(0, 2)}
+mhd = {"M10J4B4P1N1024": "/tigress/sm69/cores/mhd/M10.J4.B4.P1.N1024"}
 
 # All models
 models = {**M5N512, **M5N1024, **M10N1024, **mhd}
